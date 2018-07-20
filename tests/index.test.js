5e0d4f78dd2e484f51a588b525de61fa3d3c7bd9
afrolionx <mike@mikecorp.ca>
2018-07-20 03:06:46 -0400

    remove failing test

--- tests/index.test.js
+++ tests/index.test.js
@@ -6,8 +6,4 @@ describe('Index', () => {
   it('should export a function', () => {
     expect(typeof index).toBe('function');
   });
-
-  it('should purposefully fail', () => {
-    expect(true).toBe(false);
-  });
 });
