aecd59496cfee1a30166421730862b4fdd625a7a
afrolionx <mike@mikecorp.ca>
2018-07-20 02:26:57 -0400

    push meaningfully failing test

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,12 +1,13 @@
 'use strict';
 
-describe('Index', () => {
-  let index;
+const index = require('../index');
 
-  beforeEach(() => {
-    index = require('../index');
-  });
+describe('Index', () => {
   it('should export a function', () => {
     expect(typeof index).toBe('function');
+  });
+
+  it('should purposefully fail', () => {
+    expect(true).toBe(false);
   });
 });
